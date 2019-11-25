58c93741509b5b830eb71054a9f4689f72a18218
puoo2237 <puoo2237@gmail.com>
2019-11-25 18:16:49 +0900

    2019-11-25

--- src/homework/Homework2.java
+++ src/homework/Homework2.java
@@ -30,22 +30,37 @@ public class Homework2 {
 
 		
 		
-		// 1-100까지 소수를 구하시오
-		int count; // 나머지가 0인 값의 수
-		int jInfo; // j값 저장
-
+//		// 1-100까지 소수를 구하시오
+//		int count; // 나머지가 0인 값의 수
+//		int jInfo; // j값 저장
+//
+//		for (int i = 2; i <= 100; i++) {
+//			count = 0;
+//			jInfo = 0;
+//			for (int j = 2; j <= i; j++) {
+//
+//				if (i % j == 0) {
+//					count += 1;
+//					jInfo = j;
+//					break;
+//				}
+//			}
+//			if (count == 1 && i == jInfo) {
+//				System.out.print(i + "\t");
+//			}
+//		}
+		
+		
+		
 		for (int i = 2; i <= 100; i++) {
-			count = 0;
-			jInfo = 0;
-			for (int j = 2; j <= i; j++) {
-
+			int count = 0;
+			for (int j = 2; j < i; j++) {
 				if (i % j == 0) {
 					count += 1;
-					jInfo = j;
 					break;
 				}
 			}
-			if (count == 1 && i == jInfo) {
+			if (count == 0) {
 				System.out.print(i + "\t");
 			}
 		}
